477c51a91d936f53a588b80a932167de4d524d19
Steve Sokasits <steve.sokasits@gmail.com>
2018-09-21 09:26:43 -0500

    setup

---
+++ src/main/java/com/stevesokasits/customtags/HelloWorldTag.java
@@ -0,0 +1,16 @@
+package com.stevesokasits.customtags;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+
+public class HelloWorldTag extends SimpleTagSupport{
+
+    @Override
+    public void doTag() throws JspException, IOException {
+        super.doTag();
+        JspWriter out = getJspContext().getOut();  // send info back to the browser
+        out.println("Hello Ent Java Class");
+    }
+}
